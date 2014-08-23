cdabaad26e161b13f689806e7422e532db924af6
Atsushi Tadokoro <tadokoro@gmail.com>
2014-08-23 15:03:11 +0900

    fix indent

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -2,16 +2,16 @@
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	ofSetVerticalSync(true);
+    ofSetVerticalSync(true);
     ofSetFrameRate(60);
-	ofBackgroundHex(0x00000);
-	ofSetLogLevel(OF_LOG_NOTICE);
+    ofBackgroundHex(0x00000);
+    ofSetLogLevel(OF_LOG_NOTICE);
     
-	box2d.init();
-	box2d.setGravity(0, 10);
-	box2d.setFPS(30.0);
+    box2d.init();
+    box2d.setGravity(0, 10);
+    box2d.setFPS(30.0);
     
-	box2d.registerGrabbing();
+    box2d.registerGrabbing();
     box2d.createBounds();
     
     particles.setRadius(6.0);
@@ -28,52 +28,52 @@ void ofApp::setup() {
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	box2d.update();
+    box2d.update();
 }
 
 
 //--------------------------------------------------------------
 void ofApp::draw() {
     for(int i=0; i<circles.size(); i++) {
-		ofFill();
-		ofSetHexColor(0xf6c738);
-		circles[i].get()->draw();
-	}
-
+        ofFill();
+        ofSetHexColor(0xf6c738);
+        circles[i].get()->draw();
+    }
+    
     for(int i=0; i<boxes.size(); i++) {
-		ofFill();
-		ofSetHexColor(0xBF2545);
-		boxes[i].get()->draw();
-	}
+        ofFill();
+        ofSetHexColor(0xBF2545);
+        boxes[i].get()->draw();
+    }
     
     particles.draw();
     
     string info = "";
-	info += "Press [c] for circles\n";
-	info += "Press [b] for blocks\n";
+    info += "Press [c] for circles\n";
+    info += "Press [b] for blocks\n";
     info += "Mouse Drag for particles\n";
-	info += "Total Bodies: "+ofToString(box2d.getBodyCount())+"\n";
+    info += "Total Bodies: "+ofToString(box2d.getBodyCount())+"\n";
     info += "Total Particles: "+ofToString(particles.particleSystem->GetParticleCount())+"\n\n";
-	info += "FPS: "+ofToString(ofGetFrameRate(), 1)+"\n";
-	ofSetHexColor(0xffffff);
-	ofDrawBitmapString(info, 30, 30);
+    info += "FPS: "+ofToString(ofGetFrameRate(), 1)+"\n";
+    ofSetHexColor(0xffffff);
+    ofDrawBitmapString(info, 30, 30);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
     if(key == 'c') {
-		float r = ofRandom(4, 20);
-		circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
-		circles.back().get()->setPhysics(0.9, 0.9, 0.1);
-		circles.back().get()->setup(box2d.getWorld(), mouseX, mouseY, r);
-	}
+        float r = ofRandom(4, 20);
+        circles.push_back(ofPtr<ofxBox2dCircle>(new ofxBox2dCircle));
+        circles.back().get()->setPhysics(0.9, 0.9, 0.1);
+        circles.back().get()->setup(box2d.getWorld(), mouseX, mouseY, r);
+    }
     if(key == 'b') {
-		float w = ofRandom(20, 40);
-		float h = ofRandom(20, 40);
-		boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
-		boxes.back().get()->setPhysics(4.0, 0.53, 0.1);
-		boxes.back().get()->setup(box2d.getWorld(), mouseX, mouseY, w, h);
-	}
+        float w = ofRandom(20, 40);
+        float h = ofRandom(20, 40);
+        boxes.push_back(ofPtr<ofxBox2dRect>(new ofxBox2dRect));
+        boxes.back().get()->setPhysics(4.0, 0.53, 0.1);
+        boxes.back().get()->setup(box2d.getWorld(), mouseX, mouseY, w, h);
+    }
 }
 
 //--------------------------------------------------------------
@@ -82,7 +82,7 @@ void ofApp::keyReleased(int key) {
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ) {
-	
+    
 }
 
 //--------------------------------------------------------------
--- example/src/ofApp.h
+++ example/src/ofApp.h
@@ -5,22 +5,22 @@
 // -------------------------------------------------
 
 class ofApp : public ofBaseApp {
-	
+    
 public:
-	
-	void setup();
-	void update();
-	void draw();
-	
-	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void resized(int w, int h);
-	
-	ofxBox2d box2d;
+    
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void resized(int w, int h);
+    
+    ofxBox2d box2d;
     vector <ofPtr<ofxBox2dCircle> >	circles;		  //	default box2d circles
     vector <ofPtr<ofxBox2dRect> > boxes;			  //	defalut box2d rects
     ofxBox2dParticleSystem particles;			      //    LiquidFun particle system
